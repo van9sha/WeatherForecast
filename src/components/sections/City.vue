4955a4b032a1fdef53cce1ff1f3191ededfc0f93
van9sha <van9sha@gmail.com>
2023-09-25 22:40:57 +0300

    rework localStorage

--- src/components/sections/City.vue
+++ src/components/sections/City.vue
@@ -21,8 +21,6 @@ function getDate(){
 
 
 
-
-
 </script>
 
 <template>
--- src/composables/useLocalStorage.js
+++ src/composables/useLocalStorage.js
@@ -5,7 +5,7 @@ export default function (initialValue, key) {
     const val = ref(initialValue);
 
 
-    onMounted(() => {
+
         const storageVal = window.localStorage.getItem(key);
 
         if (storageVal) {
@@ -15,7 +15,7 @@ export default function (initialValue, key) {
         watch(val,val => {
             window.localStorage.setItem(key,JSON.stringify(val));
         }, {deep:true});
-    });
+
 
 
 
